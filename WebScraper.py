e5a23d9d631f8cf8bee6d3174477a90e7536b58b
cristicretu <cristicretu38@gmail.com>
2020-12-21 21:13:19 +0200

    check last price

--- WebScraper.py
+++ WebScraper.py
@@ -1,4 +1,5 @@
 import requests
+import sys
 from bs4 import BeautifulSoup
 
 url = 'https://www.emag.ro/laptop-apple-macbook-air-13-inch-true-tone-procesor-apple-m1-8-nuclee-cpu-si-8-nuclee-gpu-8gb-512gb-gold-int-kb-mgne3ze-a/pd/DM6BL7MBM/?ref=graph_profiled_similar_c_1_2&provider=rec&recid=rec_49_16_u2034550492514704261_91_C_ca82e2f6feb80b89b31eba3cc94e74febcb355cd836da1444147626a4132d11f_1608575391&scenario_ID=49'
@@ -6,6 +7,11 @@ url = 'https://www.emag.ro/laptop-apple-macbook-air-13-inch-true-tone-procesor-a
 headers = {
     "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'}
 # sure thing buddy
+
+
+def doSmth():
+    return 2 + 3
+    # TO DO
 
 
 def check_price():
@@ -16,7 +22,17 @@ def check_price():
     get_price = soup.find(class_="product-new-price").get_text()
     price = float(get_price[:-7])
 
-    print(price)
+    return price
 
 
-check_price()
+with open('/home/cristic/Projects/Track_Price_WebScraper/lastprice.txt', 'r') as f1:
+    last_price = float(f1.read())
+
+curr_price = check_price()
+
+if (curr_price < last_price):
+    doSmth()
+
+with open('/home/cristic/Projects/Track_Price_WebScraper/lastprice.txt', 'w') as f1:
+    sys.stdout = f1
+    print(curr_price)
